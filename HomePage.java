8e02960c05f488b260c860002e96186f06cf564b
Arpitha_Hegde <157497581+ArpithaHegde95@users.noreply.github.com>
2024-06-04 13:21:46 +0530

    Add files via upload

---
+++ HomePage.java
@@ -0,0 +1,60 @@
+package com.LinkedInHybridProject.pageObjects;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage
+{
+	WebDriver ldriver;
+	public HomePage(WebDriver rdriver)
+	{
+		ldriver=rdriver;
+		PageFactory.initElements(rdriver,this);
+	}
+	
+	@FindBy(xpath="//*[@id=\"global-nav-typeahead\"]/input")
+	@CacheLookup
+	WebElement searchbox;
+	
+	@FindBy(xpath="//*[@id=\"search-reusables__filters-bar\"]/ul/li[1]/button")
+	@CacheLookup
+	WebElement viewpage;
+	
+	@FindBy(xpath="//*[@id=\"results-list__title\"]")
+	@CacheLookup
+	WebElement textname;
+	
+	
+	@FindBy(xpath="//*[@id=\"search-reusables__filters-bar\"]/ul/li[1]/button")
+	@CacheLookup
+	WebElement peoplebtn;
+	
+	public void setSearchbox(String username)
+	{
+		searchbox.sendKeys(username);
+		searchbox.sendKeys(Keys.ENTER);
+	} 
+	
+	public void setViewPagebtn()
+	{
+		viewpage.click();
+	} 
+	
+	public void settextname()
+	{
+		if(textname.isDisplayed()==true)
+		{
+			System.out.println("Text is present under Job tab");
+		}
+	} 
+	
+	
+	public void setPeoplebtn()
+	{
+		peoplebtn.click();
+	} 
+}
